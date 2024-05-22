2edf6b01ad28302a3ab8f0c929e9ba716989b9ae
Samuel-Novoa <s4m.n0v@gmail.com>
2024-05-21 21:09:32 -0500

    Fixing logout problem in navigation bar

--- src/layout/NavigationBar.jsx
+++ src/layout/NavigationBar.jsx
@@ -1,42 +1,45 @@
-import React, { useState } from 'react';
-import { useAuth } from '../context/AuthContext';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useAuth } from "../context/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
 
 const NavigationBar = () => {
+  const auth = useAuth();
+  //   const { displayName } = auth.user;
+  const navigate = useNavigate();
 
-     const auth = useAuth()
-     const { displayName } = auth.user
+  const handleLogout = () => {
+    auth.logout();
+    navigate("/login");
+  };
 
-     const handleLogout = () => {
-          auth.logout()
-     }
-
-     return (
-          <div>
-               <nav>
-                    <div>
-                         <Link to={"/fichas"}>
-                              E&C - Gestion
-                         </Link>
-                         <ul>
-                              <li>
-                                   <Link to={"/fichas"}>Fichas</Link>
-                              </li>
-                              <li>
-                                   <Link to={"/clientes"}>Clientes</Link>
-                              </li>
-                              <li>
-                                   <Link to={"/servicios"}>Servicios</Link>
-                              </li>
-                              <li>
-                                   <Link to={"/admin"}>Admin</Link>
-                              </li>
-                              <li><button onClick={() => handleLogout()} className="button">Logout</button></li>
-                         </ul>
-                    </div>
-               </nav>
-          </div>
-     );
-}
+  return (
+    <div>
+      <nav>
+        <div>
+          <Link to={"/fichas"}>E&C - Gestion</Link>
+          <ul>
+            <li>
+              <Link to={"/fichas"}>Fichas</Link>
+            </li>
+            <li>
+              <Link to={"/clientes"}>Clientes</Link>
+            </li>
+            <li>
+              <Link to={"/servicios"}>Servicios</Link>
+            </li>
+            <li>
+              <Link to={"/admin"}>Admin</Link>
+            </li>
+            <li>
+              <button onClick={() => handleLogout()} className="button">
+                Logout
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 export default NavigationBar;
